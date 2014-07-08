b12d28704437d3b847e1a228a374dce36b14a623
Isaiah <isaiah@atpay.com>
2014-07-08 17:03:20 -0600

    Fix Failing tests for site tokens

--- src/AtPay/Tokenizer.php
+++ src/AtPay/Tokenizer.php
@@ -40,7 +40,7 @@
 
       $contents = $nonce->nbin . $partner_id . $header_length . $header_box . $ip_length . $ip . $body;
 
-      return "@" . $this->encode64($contents);
+      return "@".$this->encode64($contents)."@";
     }
 
 
@@ -75,7 +75,6 @@
 
     private function encode64($data)
     {
-      // return base64_encode($data);
       return strtr(base64_encode($data), '+/=', '-_');
     }
 
@@ -114,15 +113,15 @@
         $amount = $params["amount"];
       }
 
-        if($params["type"] == "url") {
-          $body = "url<" . $target . ">";
-        }elseif($params["type"] == "email"){
-          $body = "email<" . $target . ">";
-        }elseif($params["type"] == "member"){
-          $body = "member<" . $target . ">";
-        }else{
-          $body = "card<" . $target . ">";
-        }
+      if($params["type"] == "url") {
+        $body = "url<" . $target . ">";
+      }elseif($params["type"] == "email"){
+        $body = "email<" . $target . ">";
+      }elseif($params["type"] == "member"){
+        $body = "member<" . $target . ">";
+      }else{
+        $body = "card<" . $target . ">";
+      }
 
 
       if(array_key_exists("group", $params)) {
--- test/TokenizerTest.php
+++ test/TokenizerTest.php
@@ -2,7 +2,7 @@
   require __DIR__ . "/helper.php";
 
   /**
-  * 
+  *
   */
   class TokenizerTest extends PHPUnit_Framework_TestCase
   {
@@ -12,7 +12,7 @@
     public function testSiteTokenDefaults()
     {
       $keys = [
-        "private" => "EpBic6szxPJVbwlW3VAfEzMZSdWdA04t2Nm6yRQFpf0=", 
+        "private" => "EpBic6szxPJVbwlW3VAfEzMZSdWdA04t2Nm6yRQFpf0=",
         "public" => "jZutz9bU6FWIIcRn/12zneT74yWCCuvN5/Su5LvP+3o=",
         "atpay" => "x3iJge6NCMx9cYqxoJHmFgUryVyXqCwapGapFURYh18="
       ];
@@ -21,7 +21,8 @@
 
       $card = "OTAzYzUzNWVjOVKhtOalUQA=";
       $tokenizer = new \AtPay\Tokenizer($keys);
-      $this->token = base64_decode(ltrim($tokenizer->site_token($card, $this->params()), "@"));
+      $val = ltrim($tokenizer->site_token($card, $this->params()), "@");
+      $this->token = base64_decode(strtr($val, '-_,', '+/='));
 
       $this->has_ip($this->params()["ip"]);
       $this->has_partner_id($this->params()["partner_id"]);
@@ -30,13 +31,13 @@
     private function params()
     {
       return [
-        "ip" => "173.163.242.213", 
+        "ip" => "173.163.242.213",
         "headers" => [
-          "user_agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36", 
-          "accept_lang" => "en-US,en;q=0.8", 
+          "user_agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
+          "accept_lang" => "en-US,en;q=0.8",
           "accept_chars" => ""
-        ], 
-        "partner_id" => 19, 
+        ],
+        "partner_id" => 19,
         "card" => "OTAzYzUzNWVjOVKhtOalUQA="
       ];
     }

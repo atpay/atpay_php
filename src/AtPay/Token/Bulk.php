2b0e0c5e7f1c2f8a6c42e3cbdd212e19a70753b2
Isaiah <isaiah@atpay.com>
2014-07-14 07:33:42 -0600

    Adds Expiration, Auth Only and User Data attributes to Invoice and Bulk classes

--- src/AtPay/Token/Bulk.php
+++ src/AtPay/Token/Bulk.php
@@ -13,16 +13,30 @@
       $this->amount = $amount;
       $this->nonce = $session->noncer->next();
       $this->partner_id = $session->packer->big_endian_long($session->partner_id);
+      $this->version = null;
+      $this->expiration = time() + (60 * 60 * 24 * 7);
+      $this->user_data = "{'ref_id' : '".$ref."'}";
+    }
+
+    public function auth_only()
+    {
+      $this->version = "2~";
+    }
+
+    public function expires_in_seconds($seconds)
+    {
+      $this->expiration = $seconds;
+    }
+
+    public function user_data($string)
+    {
+      $this->user_data = $string
     }
 
     public function to_s(){
-
       $body = $this->box($this->build_body($this->amount, $this->url), $this->nonce);
-
       $contents = $this->nonce->nbin . $this->partner_id . $body;
-
-      return "@".$this->encode64($contents)."@";
-
+      return "@".$this->version.$this->encode64($contents)."@";
     }
 
     private function box($payload, $nonce)
@@ -35,12 +49,13 @@
       return strtr(base64_encode($data), '+/', '-_');
     }
 
-    private function build_body($amount, $url=null)
+    private function build_body($amount, $url)
     {
-      $expiration = time() + (60 * 60 * 24 * 7);
       $body = "url<" . $url . ">";
       $body .= "/" . $this->packer->big_endian_float($amount);
       $body .= $this->packer->big_endian_signed_32bit($expiration);
+      $body .= "/" . $this->user_data;
+
       return $body;
     }
   }
--- src/AtPay/Token/Invoice.php
+++ src/AtPay/Token/Invoice.php
@@ -13,13 +13,31 @@
       $this->amount = $amount;
       $this->nonce = $session->noncer->next();
       $this->partner_id = $session->packer->big_endian_long($session->partner_id);
+      $this->version = null;
+      $this->expiration = time() + (60 * 60 * 24 * 7);
+      $this->user_data = "{'ref_id' : '".$ref."'}";
+    }
+
+    public function auth_only()
+    {
+      $this->version = "2~";
+    }
+
+    public function expires_in_seconds($seconds)
+    {
+      $this->expiration = $seconds;
+    }
+
+    public function user_data($string)
+    {
+      $this->user_data = $string
     }
 
     public function to_s()
     {
       $body = $this->box($this->build_body($this->amount, $this->email), $this->nonce);
       $contents = $this->nonce->nbin . $this->partner_id . $body;
-      return "@".$this->encode64($contents)."@";
+      return "@".$this->version.$this->encode64($contents)."@";
     }
 
     private function box($payload, $nonce)
@@ -34,10 +52,11 @@
 
     private function build_body($amount, $email)
     {
-      $expiration = time() + (60 * 60 * 24 * 7);
       $body = "email<" . $email . ">";
       $body .= "/" . $this->packer->big_endian_float($amount);
       $body .= $this->packer->big_endian_signed_32bit($expiration);
+      $body .= "/" . $this->user_data;
+
       return $body;
     }
   }
